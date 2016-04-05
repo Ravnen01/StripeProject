c1fd5bfcdf8f47ca6d9c09461e1bdb17ef27cb49
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-04-05 17:09:31 +0200

    touch id

---
+++ IosProject/StripeProject/StripeProject/PaymentController.swift
@@ -0,0 +1,13 @@
+//
+//  PaymentController.swift
+//  StripeProject
+//
+//  Created by Corentin LECOMTE on 29/03/2016.
+//  Copyright © 2016 LECOR. All rights reserved.
+//
+
+import Cocoa
+
+class PaymentController: UIViewController {
+
+}
